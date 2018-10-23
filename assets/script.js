a320ab5df4eae7a60a281ad526d69f598148a036
Evan Henry <evan.henry.dev@gmail.com>
2018-10-23 12:08:42 -0400

    cleanup

---
+++ assets/script.js
@@ -0,0 +1,85 @@
+function login() {
+    $.ajax({
+        url: "/home",
+        type: 'POST',
+        dataType: 'json', // added data type
+        data: {
+            username: $("#username").val(),
+            password: $("#password").val()
+        },
+        success: function (data) {
+            $("#profile_pic").attr("src", data['profile_pic']);
+            console.log(data['friends']);
+            buildFriendsList(JSON.parse(data['friends']));
+            buildMessageList(JSON.parse(data['friends']));
+            $("body").toggleClass("dialogIsOpen");
+            $('#modal1').modal('hide');
+            return;
+        }
+    });
+}
+
+function buildFriendsList(data) {
+    var conn = '<li href="#" class="list-group-item title">Your Connections</li>';
+    for (var i = 0; i < Object.keys(data).length; i++) {
+        conn = conn + '<li href="#" class="list-group-item text-left"><img class="img-thumbnail" src="' + data[i]['profile_pic'] + '"><label class="name">' + data[i]['username'] + '<br></label><label class="pull-right"><a class="btn btn-success btn-xs glyphicon glyphicon-user" href="#" title="View"></a></label><div class="break"></div></li>';
+    }
+    $('#connections_list').html(conn);
+}
+
+function buildMessageList(data) {
+    var conn = '<li href="#" class="list-group-item title">Your Messages</li>';
+    for (var i = 0; i < Object.keys(data).length; i++) {
+        if(data[i]['messages'] != null){
+            GLOBAL_MESSAGES[data[i]['username']] = data[i]['messages'];
+            conn = conn + '<li href="#" class="list-group-item text-left"><img class="img-thumbnail" src="'+data[i]['profile_pic']+'"><label class="pull-right"><a class="btn btn-success btn-xs glyphicon glyphicon-envelope" href="#" title="View"></a></label><label class="nameMessage">'+data[i]['username']+'</label><label class="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></label><div class="break"></div></li>;
+        }
+    }
+    $('#connections_list').html(conn);
+}
+
+function openMessaging(){
+    var message_btn = '<div class="row"><form><div class="input-group"><input type="text" class="form-control" placeholder="Search" id="txtSearch" /><div class="input-group-btn"><button class="btn btn-primary" type="submit">Send</button></div></div></form></div>';
+    var message_box = '<div id="center-pane"></div>';
+    
+    //for(var i = 0; i < )
+}
+
+
+
+
+
+// JQuery
+$(document).ready(function () {
+    $("#modal").modal('show');
+    $("body").toggleClass("dialogIsOpen");
+    $("#Modal-Login").show()
+    $("#Modal-Sign-Up").hide();
+    $("#Landing-Center").show().fadeIn("fast");
+    $("#Center-Selection").hide();
+});
+
+function newUser() {
+    $("#Modal-Login").hide();
+    $("#Modal-Sign-Up").show();
+}
+
+function CancelLogin() {
+    $("#modal").modal('hide');
+    $("body").toggleClass("dialogIsOpen");
+}
+
+function SelectionToHome() {
+    $("#Landing-Center").show();
+    $("#Center-Selection").hide();
+}
+
+function Selection() {
+    $("#Landing-Center").hide();
+    $("#Center-Selection").show();
+}
+
+$("#logout-btn").on("click", function() {
+    $("#modal").modal('show');
+    $("body").toggleClass("dialogIsOpen");
+})
--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-//const Cryptr   = require('cryptr');
-//const cryptr   = new Cryptr('BaCoNaNdEgGsArEsOgOoD');
 var express = require('express');
 var app = express();
-//var mysql      = require('mysql');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
-var Pusher = require('pusher');
 const path = require('path');
-const Chatkit = require('pusher-chatkit-server');
-
-/*var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'Weaves'
-});*/
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
 
 // Setup
 app.use(bodyParser.json()); // support json encoded bodies
@@ -21,24 +14,27 @@ app.use(bodyParser.urlencoded({
     extended: true
 })); // support encoded bodies
 app.use(express.static(path.join(__dirname, 'assets')));
-//app.use('/styles', express.static(__dirname + 'public/css'));
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var pusher = new Pusher({
-    appId: '626390',
-    key: 'd3b35616dbc5e2ec8d96',
-    secret: '1fba85ea02a91d87bad2',
-    cluster: 'us2',
-    encrypted: true
-});
+// Variables
+var port = process.env.PORT || 3000;
+var home = "home.html";
+var CONNECTED_USERS = [];
 
-const chatkit = new Chatkit.default({
-    instanceLocator: "v1:us1:46a189e4-53c7-4620-a608-5205b8536b7d",
-    key: "75e5d00e-029d-4490-afe1-0d291c1e3fb5:iK3SMLpnfv3TnbtLxI11y2TrbSIKb5fDDUIZy9uy3SY="
-});
-
-var home = "test.html";
+// Encryption/Decryption
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+//////////////////////////
 
 // sends landing page when you first load webpage
 app.get("/", function (req, res) {
@@ -46,8 +42,6 @@ app.get("/", function (req, res) {
         root: __dirname + "/public"
     });
 });
-
-
 
 //checks login info then sends data to user and redirects to home page
 /*app.post("/home", function(req, res){
@@ -60,15 +54,15 @@ app.get("/", function (req, res) {
         else{
             var data = results[0];
             res.json({fname: data['fname'], lname: data['lname'], email: data['email'], username: data['username'], phone: data['phone'], origin: data['origin'], profile_pic: data['profile_pic'], friends: data['friends_list']});
-            res.emit('close');
+            res.emit('OK!');
             res.end();
         }
     });
     connection.end();
-});*/
+});
 
 // registers new user
-/*app.post("/register", function(req, res) {
+app.post("/register", function(req, res) {
     
     var query_string = `INSERT INTO Users (fname, lname, email, username, password, phone, origin) VALUES ('${req.body.fname}', '${req.body.lname}', '${req.body.email}', '${req.body.username}', '${cryptr.encrypt(req.body.password)}', '${req.body.phone}', '${req.body.origin}');`;
     
@@ -81,77 +75,12 @@ app.get("/", function (req, res) {
     
 });*/
 
-app.post('/session/load', (req, res, next) => {
-    // Attempt to create a new user with the email serving as the ID of the user.
-    // If there is no user matching the ID, we create one but if there is one we skip
-    // creating and go straight into fetching the chat room for that user
-    chatkit.createUser(req.body.email, req.body.name)
-        .then(() => getUserRoom(req, res, next))
-        .catch(err => {
-            (err.error_type === 'services/chatkit/user/user_already_exists') ?
-            getUserRoom(req, res, next): next(err)
-        })
-
-    function getUserRoom(req, res, next) {
-        const name = req.body.name
-        const email = req.body.email
-
-        // Get the list of rooms the user belongs to. Check within that room 
-        // list for one whos name matches the users ID. If we find one, we 
-        // return that as the response, else we create the room and return 
-        // it as the response.
-        chatkit.apiRequest({
-                method: 'GET',
-                'path': `/users/${email}/rooms`
-            })
-            .then(rooms => {
-                let clientRoom = false
-
-                // Loop through user rooms to see if there is already a room for 
-                // the client
-                rooms.forEach(room => {
-                    return room.name === email ? (clientRoom = room) : false
-                })
-
-                if (clientRoom && clientRoom.id) {
-                    return res.json(clientRoom)
-                }
-
-                const createRoomRequest = {
-                    method: 'POST',
-                    path: '/rooms',
-                    jwt: chatkit.generateAccessToken({
-                        userId: email
-                    }).token,
-                    body: {
-                        name: email,
-                        private: false,
-                        user_ids: ['adminuser']
-                    },
-                };
-
-                // Since we can't find a client room, we will create one and return 
-                // that.
-                chatkit.apiRequest(createRoomRequest)
-                    .then(room => res.json(room))
-                    .catch(err => next(
-                        new Error(`${err.error_type} - ${err.error_description}`)
-                    ))
-            })
-            .catch(err => next(
-                new Error(`${err.error_type} - ${err.error_description}`)
-            ))
-    }
-})
-
-app.post('/session/auth', (req, res) => {
-    res.json(chatkit.authenticate(req.body, req.query.user_id))
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-pusher.trigger('my-channel', 'my-event', {
-    "message": "hello world"
-});
-
-app.listen(server_port, server_ip_address, function () {
-    console.log("Listening on " + server_ip_address + ", port " + server_port)
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
